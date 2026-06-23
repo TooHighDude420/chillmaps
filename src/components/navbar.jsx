bc19182243398d375a24675eae7d3632a1fd63f3
TooHighDude420 <nataro112@gmail.com>
2026-06-23 15:00:41 +0200

    navbar displays avatar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,8 +6,22 @@ import postIco from '../assets/post.svg';
 import mapIco from '../assets/map.svg';
 import profileIco from '../assets/profile.svg';
 import databaseCallList from "../api";
+import { useEffect, useState } from "react";
 
 function Navbar() {
+    const [profilePic, setprofilePic] = useState(null);
+
+    useEffect(() => {
+        databaseCallList.getUser().then((msg) => {
+            databaseCallList.getOwnUser(msg.id).then((data) => {
+                databaseCallList.getFromStorage(data[0].Images.bucket, data[0].Images.path).then((mess) => {
+                    setprofilePic(mess);
+                })
+            })
+        })
+    }, [])
+
+
     if (useLocation().pathname == "/login") {
         return (
             <div className="h-[10vh] w-full bg-[#021A1C]">
@@ -21,9 +35,7 @@ function Navbar() {
                 <Link to={'/chat'}><img src={chatIco} className="size-16"></img></Link>
                 <Link to={'/post'}><img src={postIco} className="size-16"></img></Link>
                 <Link to={'/map'}><img src={mapIco} className="size-16"></img></Link>
-                {databaseCallList.getLoggedIn() != null ?
-                    <Link to={'/profile'}><img src={profileIco} className="size-16"></img></Link>
-                    : <Link to={'/login'}><img src={profileIco} className="size-16"></img></Link>}
+                <Link to={'/profile'}><img src={profilePic ? profilePic : profileIco} className="size-16 rounded-full"></img></Link>
             </div>
         )
     }
